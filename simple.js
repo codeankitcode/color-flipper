6489b2e455fd5198ac2bee0e379398699cca0f37
Ankit Kumar Roy <ankitdasout@gmail.com>
2023-01-14 14:55:47 +0530

    styles
    design is ready

---
+++ simple.js
@@ -0,0 +1,19 @@
+// const btn = document.querySelector("#btn")
+// const color = document.querySelector("#color")
+// console.log(color)
+
+
+// let colors = ["FAD3E7", "EFA3C8", "0081B4", "FF8B13", "8DCBE6", "FFEA20", "13005A"]
+
+// btn.addEventListener("click", function(){
+//     let colorCode = "#"
+    
+// })
+
+// function randomColor(){
+//     let randomNum = Math.floor(Math.random() * 7)
+//     return randomNum
+// }
+
+
+// console.log(randomColor())
